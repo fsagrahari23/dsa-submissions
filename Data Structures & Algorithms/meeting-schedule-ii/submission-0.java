96824cd537c4840e3b7a9bb4c6f1c5cad5c8f914
codewithmonu <monu.a23@iiits.in>
2026-09-24 10:16:05 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.java
@@ -0,0 +1,53 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+          Collections.sort(intervals,(a,b)->{
+             if(a.start==b.start)return a.end-b.end;
+             return a.start-b.start;
+          });
+          int s = intervals.get(0).start;
+          int e = intervals.get(0).end;
+
+          int count = 1;
+
+          for(int i=1;i<intervals.size();i++){
+            int cs = intervals.get(i).start;
+            int ce = intervals.get(i).end;
+            if(e>cs){
+                count++;
+                e=Math.min(e,ce);
+            }else{
+                e=ce;
+                s=cs;
+            }
+
+          }
+
+
+          return count;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
